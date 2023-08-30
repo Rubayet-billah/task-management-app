2d8ca9e309c548a991f4bceb29c35fb77ce1bdae
Rubayet-billah <rubayetbillah909@gmail.com>
2023-08-30 21:19:47 +0600

    task displayed using redux

--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -1,10 +1,13 @@
+import { useSelector } from "react-redux";
+
 /* eslint-disable react/prop-types */
-function TaskList({ tasks }) {
+function TaskList() {
+  const tasks = useSelector((state) => state.tasks);
   return (
     <ul className="bg-gray-100 px-4 rounded-lg shadow mt-5">
-      {tasks.map((task, index) => (
+      {tasks?.map((task, index) => (
         <li key={index} className="border-b-2 py-2">
-          {task}
+          {task.text}
         </li>
       ))}
     </ul>
